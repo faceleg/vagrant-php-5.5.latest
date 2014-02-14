0ca82b73974616b0ccdefb6f7ed9f980fdccc538
Michael Robinson <mike@pagesofinterest.net>
2014-02-14 18:35:03 +1300

    Updated PHP

--- configuration/php.pp
+++ configuration/php.pp
@@ -1,19 +1,26 @@
-class php54 {
+class php55 {
   file { "/etc/apt/sources.list":
     ensure => file,
     owner => root,
     group => root,
     source => "/configuration/apt/sources.list",
   }
+
   exec { "import-gpg":
     command => "/usr/bin/wget -q http://www.dotdeb.org/dotdeb.gpg -O -| /usr/bin/apt-key add -"
   }
 
-  exec { "/usr/bin/apt-get update":
-    require => [File["/etc/apt/sources.list"], Exec["import-gpg"]],
+  exec { "apt-get update":
+    command => "/usr/bin/apt-get update",
+    require => [
+      File["/etc/apt/sources.list"],
+      Exec["import-gpg"]
+    ],
   }
 
 }
+
+class { "php55": }
 class { "php":
   source_dir => '/configuration/php/',
   source_dir_purge => false, # Set to true to purge any existing file not present in $source_dir
@@ -23,11 +30,6 @@ php::module { "mysql": }
 php::module { "mcrypt": }
 php::module { "gd": }
 php::module { "curl": }
-
-php::pear::module { "Zend":
-  repository  => "zend.googlecode.com/svn",
-  use_package => "no",
-}
 
 php::pecl::module { 'xdebug':
   use_package => "no",
