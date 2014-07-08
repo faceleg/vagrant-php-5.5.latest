ad6f5aa26276c2398b48deea7e24166c1534a009
Michael Robinson <mike@pagesofinterest.net>
2014-07-08 18:38:13 +1200

    Use latest version of php

--- configuration/php.pp
+++ configuration/php.pp
@@ -1,26 +1,7 @@
-class php55 {
-  file { "/etc/apt/sources.list":
-    ensure => file,
-    owner => root,
-    group => root,
-    source => "/configuration/apt/sources.list",
-  }
+include apt
 
-  exec { "import-gpg":
-    command => "/usr/bin/wget -q http://www.dotdeb.org/dotdeb.gpg -O -| /usr/bin/apt-key add -"
-  }
+apt::ppa { 'ppa:ondrej/php54': }
 
-  exec { "apt-get update":
-    command => "/usr/bin/apt-get update",
-    require => [
-      File["/etc/apt/sources.list"],
-      Exec["import-gpg"]
-    ],
-  }
-
-}
-
-class { "php55": }
 class { "php":
   source_dir => '/configuration/php/',
   source_dir_purge => false, # Set to true to purge any existing file not present in $source_dir
