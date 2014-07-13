eb79ddbd226fb6e4bdda5ce89592391459af7735
Michael Robinson <mike@pagesofinterest.net>
2014-07-14 11:34:54 +1200

    14.04 has 5.5 already

--- configuration/php.pp
+++ configuration/php.pp
@@ -1,10 +1,12 @@
 include apt
 
-apt::ppa { 'ppa:ondrej/php54': }
+/* apt::ppa { 'ppa:ondrej/php5': } */
 
 class { "php":
+  /* require => Apt::Ppa['ppa:ondrej/php5'], */
   source_dir => '/configuration/php/',
   source_dir_purge => false, # Set to true to purge any existing file not present in $source_dir
+  /* version => '5.5.14+dfsg-2+deb.sury.org~precise+1' */
 }
 
 php::module { "mysql": }
