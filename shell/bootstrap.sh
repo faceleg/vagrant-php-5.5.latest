228ec6ce1fd0adb73849a0355f4c4fd531d3620d
Michael Robinson <mike@pagesofinterest.net>
2014-07-14 11:43:36 +1200

    Install librarian puppet

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -2,14 +2,12 @@
 export DEBIAN_FRONTEND=noninteractive
 
 APT_GET=/usr/bin/apt-get
-$APT_GET -q -y install git puppet
+$APT_GET -q -y install git puppet librarian-puppet
 
 PUPPET_DIR='/vagrant/puppet'
 if [ `gem query --local | grep librarian-puppet | wc -l` -eq 0 ]; then
-  gem install librarian-puppet
   cd $PUPPET_DIR && librarian-puppet install --clean
 else
-  gem update
   cd $PUPPET_DIR && librarian-puppet update
 fi
 
